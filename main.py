439bc5d0fdb758e312409c3c59d0ff50894178e4
Bulut <bulutk@gmail.com>
2025-06-20 12:01:37 +0100

    Handle empty query strings

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from google.appengine.ext.webapp import template
 
 class MainHandler(webapp.RequestHandler):
   def get (self, q):
-    if q is None:
+    if not q:
       q = 'index.html'
 
     path = os.path.join (os.path.dirname (__file__), q)
